b0c0f1d9eb466494f4eb54419e69c9a541e988bc
Vinita Kumari <vinita.kumari@qubited.com>
2023-08-10 10:03:53 +0530

    association fixed

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,4 @@
 class Subscription < ApplicationRecord
-  has_one :user_subscription, dependent: :destroy
-  has_one :user, through: :user_subscription
+  has_many :user_subscription, dependent: :destroy
+  has_many :user, through: :user_subscription
 end
